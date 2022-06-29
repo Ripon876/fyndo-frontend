ae8ccb38b301e11a306a38523c5b40554c7133cf
MD Ripon Islam <islam876ripon@gmail.com>
2022-06-30 00:07:59 +0600

    Toast alert moveed out

---
+++ src/utils/ToastAlert.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const MySwal = withReactContent(Swal)
+
+
+
+
+
+function  Toast({type,icon,title}) {
+
+		const Toast = MySwal.mixin({
+		  toast: true,
+		  position: 'top-end',
+		  iconColor: '#9ca3af',
+		  showConfirmButton: false,
+		  timer: 2500,
+		  customClass: {
+			   popup: 'colored-toast'
+			},
+		 timerProgressBar: true
+		})
+
+		 Toast.fire({
+		  type: type,
+		  icon : icon,
+		  title: title
+		})
+   
+
+// return Toast;
+
+}
+
+export default Toast;
