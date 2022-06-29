c87f474a76802073de023573a70595994d17ef3f
MD Ripon Islam <islam876ripon@gmail.com>
2022-06-29 18:05:23 +0600

    added link to profile with image

--- src/components/newpost/NewPost.jsx
+++ src/components/newpost/NewPost.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
 import './NewPost.css';
 import Form  from './Form';
 
@@ -19,7 +20,7 @@ const closeModal = () => {
             <div className="row height d-flex justify-content-center align-items-center">
               <div className="col-md-6 col-sm-9">
                 <div className="form d-flex">
-                  <img className='me-2 p-1 profileImg' src="https://via.placeholder.com/50" alt="Id name" />
+                  <Link to='/profile' ><img className='me-2 p-1 profileImg' src="https://via.placeholder.com/50" alt="Id name" /></Link>
                   <input type="text" className="form-control form-input" placeholder="Write something.." onClick={()=> { setShowModal(!showModal) }} />
                 </div>
               </div>
