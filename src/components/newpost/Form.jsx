39e6483f9dceacde287e52a4a2c7265a4f9b9ed3
MD Ripon Islam <islam876ripon@gmail.com>
2022-06-30 01:02:05 +0600

    post form updated

--- src/components/newpost/Form.jsx
+++ src/components/newpost/Form.jsx
@@ -76,7 +76,7 @@ if(profile){
 		<>
 		<Fade duration={500} >
 			<div className="modal d-block row" id="exampleModalCenter" tabindex="-1" >
-				<div className="col-7 m-auto modal-dialog-centered px-5" role="document">
+				<div className="col-md-7 col-sm-10 m-auto modal-dialog-centered px-5" role="document">
 					<div className="modal-content p-3">
 						<h1 className='small text-end closeModal'  onClick={close} ><i class="fa-solid fa-xmark"></i></h1>
 						<div className="modal-body">
@@ -85,7 +85,7 @@ if(profile){
 								<label for="exampleInputPassword1" className="form-label">Write what you want</label>
                
              	 				<EmojiPopUp f={addEmoji} />
-								<textarea onChange={(e) => setInput(  e.target.value)} className='form-control' id="" cols="30" rows="10" value={input} />
+								<textarea autoFocus  onChange={(e) => setInput(  e.target.value)} className='form-control' id="" cols="30" rows="10" value={input} />
 								</div>
 								<button  onClick={post} className="btn t-btn">Post</button>
 							</div>
