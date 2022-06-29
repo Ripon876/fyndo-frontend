cf2fa39d49831a8eb8130d08a7acb1422172995f
MD Ripon Islam <islam876ripon@gmail.com>
2022-06-29 13:23:33 +0600

    profile picture added

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -32,10 +32,13 @@ useEffect(() => {
 <div className="container">
 	<div className="row">
 		<div className="col-10 m-auto">
-		<div className="profileHeader">
+		<div className="profileHeader position-relative">
 			<div className='coverPhoto position-relative'>
 				<img src="https://via.placeholder.com/650x300" className='img-fluid w-100' alt="Cover Photo" />
 			    <div className='bottom-0 coverFadeBottom position-absolute w-100'></div>
+			</div>
+			<div className='profilePhoto'>
+				<img src="https://via.placeholder.com/200x200" alt=""  className='rounded-circle w-100' />
 			</div>
 		</div>
 			<h1>dfdsfdsfd</h1>
