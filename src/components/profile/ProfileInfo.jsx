5f241abcac60a35ae96317376b614c802167b65a
MD Ripon Islam <islam876ripon@gmail.com>
2022-07-01 16:19:07 +0600

    profile info updated

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -7,6 +7,12 @@ function ProfileInfo() {
 				    <p className='font-monospace text-center'>hello world!</p>
 			    </div>
 			    <div className="bio p-2 border-start">
+				    <h5 className='ps-2'>Contacts</h5>
+				       <p className='font-monospace text-start'><i class="fa-solid fa-phone"></i> <span>+880123456789</span> </p>
+				       <p className='font-monospace text-start'><i class="fa-solid fa-envelope"></i> <span>sdfa@gmail.com</span> </p>
+				       <p className='font-monospace text-start'><i class="fa-brands fa-github"></i> <span>sdfdsf</span> </p>
+			    </div>
+			    <div className="bio p-2 border-start">
 				    <h5 className='ps-2'>Education</h5>
 				    <p className='font-monospace text-start'>Lorem ipsum dolor sit amet consectetur - <span>status</span> </p>
 			    </div>
