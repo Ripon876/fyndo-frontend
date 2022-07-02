60025e156ad7ddb6d8f0de9066fce3394a575bd2
MD Ripon Islam <islam876ripon@gmail.com>
2022-07-02 18:10:38 +0600

    settings page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignUp from  './components/signup/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import MessagesPage from './pages/MessagesPage';
 import ProfilePage from './pages/ProfilePage';
+import SettingPage from './pages/SettingPage';
 import AuthProtected from './auth/AuthProtected';
 import NotFound from './components/notfound/NotFound';
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/' element={ <AuthProtected path='/' ><Dashboard /></AuthProtected>} />
           <Route path='/messages' element={ <AuthProtected path='/messages'><MessagesPage /></AuthProtected>} />
           <Route path='/profile' element={ <AuthProtected path='/profile'><ProfilePage /></AuthProtected>} />
+          <Route path='/settings' element={ <AuthProtected path='/settings'><SettingPage /></AuthProtected>} />
         
             
             <Route path='/signup' element={<SignUp />} />
---
+++ src/pages/SettingPage.jsx
@@ -0,0 +1,13 @@
+import Setting from '../components/setting/Setting';
+import Sidebar from '../components/sidebar/Sidebar';
+
+function SettingPage() {
+	return (
+		<>
+			<Sidebar />
+			<Setting />
+		</>
+	)
+}
+
+export default SettingPage
