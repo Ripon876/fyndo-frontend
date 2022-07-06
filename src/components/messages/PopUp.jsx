47f5e00368aceea60bc4572dedc48c05f7f43d2f
MD Ripon Islam <islam876ripon@gmail.com>
2022-07-06 12:53:30 +0600

    closing popup on outside click of image

--- src/components/messages/PopUp.jsx
+++ src/components/messages/PopUp.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import ClickOutside from 'react-click-outside';
+
+
+
+
 
 function PopUp({imgSrc,closePopUP}) {
 	return (
 		<div className='img-popup'>
 			<div className="popUpContent">
-				<div>
-					<i className="fa fa-xmark mb-2 clearImg" onClick={closePopUP} ></i>
-					<div className='w-75 m-auto'>
-						<img src={imgSrc} className='img-fluid' />
+			    <ClickOutside onClickOutside={closePopUP}>
+					<div>
+						<i className="fa fa-xmark mb-2 clearImg" onClick={closePopUP} ></i>
+						<div className='w-75 m-auto'>
+							<img src={imgSrc} className='img-fluid' />
+						</div>
 					</div>
-				</div>
+				</ClickOutside>
 			</div>
 		</div>
 	)
