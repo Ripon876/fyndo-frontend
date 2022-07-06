8668ba22188a208857774065ae3c68b621ad36e3
MD Ripon Islam <islam876ripon@gmail.com>
2022-07-07 01:37:39 +0600

    showing contacts dynamically

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -6,17 +6,17 @@ function ProfileInfo({user}) {
 			<div className="col-4">
 			    <div className="bio p-2 border-start mt-0">
 				    <h5 className='ps-2'>Intro</h5>
-				    <p className='font-monospace text-center'>{user?.bio}</p>
+				    <p className='font-monospace text-center lead '>{user?.bio}</p>
 			    </div>
 			    <div className="bio p-2 border-start">
 				    <h5 className='ps-2'>Contacts</h5>
-				       <p className='font-monospace text-start'><i className="fa-solid fa-phone"></i> <span>+880123456789</span> </p>
-				       <p className='font-monospace text-start'><i className="fa-solid fa-envelope"></i> <span>sdfa@gmail.com</span> </p>
-				       <p className='font-monospace text-start'><i className="fa-brands fa-github"></i> <span>sdfdsf</span> </p>
+				       <p className='font-monospace text-start ps-1'><i className="fa-solid fa-phone"></i> <span>{user?.contacts?.phone_num}</span> </p>
+				       <p className='font-monospace text-start ps-1'><i className="fa-solid fa-envelope"></i> <span>{user?.contacts?.email}</span> </p>
+				       <p className='font-monospace text-start ps-1'><i className="fa-solid fa-location-dot"></i> <span>{user?.contacts?.address}</span> </p>
 			    </div>
 			    <div className="bio p-2 border-start">
 				    <h5 className='ps-2'>Education</h5>
-				    <p className='font-monospace text-start'>Lorem ipsum dolor sit amet consectetur - <span>status</span> </p>
+				    <p className='font-monospace text-start ps-1'>Lorem ipsum dolor sit amet consectetur - <span>status</span> </p>
 			    </div>
 				
 			</div>
