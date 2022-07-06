53fc448e24b78ae80db44ecc50b4fda270ccddf5
MD Ripon Islam <islam876ripon@gmail.com>
2022-07-07 01:16:29 +0600

    contacts now can be updated

--- src/components/setting/Contacts.jsx
+++ src/components/setting/Contacts.jsx
@@ -19,6 +19,10 @@ import Toast from '../../utils/ToastAlert';
 
 function Contacts() {
 
+
+
+const token =  useRecoilValue(authToken);
+const user = jwt_decode(token);
 const [contacts,setContacts] = useState({
 	phone_num : '',
 	email : '',
@@ -28,8 +32,51 @@ const [contacts,setContacts] = useState({
 
 
 
+useEffect(() => {
+	
+socket.emit('getUserInfo',user.id,(res) => {
+	if(res.status){
+		setContacts({
+			phone_num : res.data?.contacts?.phone_num ?? '',
+			email :  res.data?.contacts?.email ?? '',
+			address :  res.data?.contacts?.address ?? ''
+		})
+	}
+	
+})
+
+}, [])
+ 
 
 
+
+const saveContacts = ()=>{
+	
+	if(Object.values(contacts).some((i ) => i !== '' )){
+		socket.emit('saveContacts',user.id,contacts,(res)=> {
+			if(res.status){
+				setContacts({
+					phone_num : res.data?.contacts?.phone_num,
+					email :  res.data?.contacts?.email,
+					address :  res.data?.contacts?.address
+				});
+				Toast({
+					type: 'success',
+					icon : 'success',
+					title : 'Contacts updated'
+				})
+			}else{
+				Toast({
+					type: 'error',
+					icon : 'error',
+					title : 'Something went wrong'
+				})
+			};
+		});
+	};
+
+
+};
 
 
 	return (
@@ -41,24 +88,24 @@ const [contacts,setContacts] = useState({
 							<div className="mb-3">
 								<label  className="form-label">Phone Number</label>
 								<div className='d-flex gap-3'>
-								  	<input type="number"  defaultValue={contacts?.phone_num}   className="form-control"  placeholder='number' />
+								  	<input type="number"  defaultValue={contacts?.phone_num} onChange={(e)=> { setContacts({...contacts,phone_num : e.target.value}) }}  className="form-control"  placeholder='number' />
 								</div>  
 							</div>
 							<div className="mb-3">
 								<label  className="form-label">Email</label>
 								<div className='d-flex gap-3'>
-								  	<input type="email"   defaultValue={contacts?.email}   className="form-control"  placeholder='email' />
+								  	<input type="email"   defaultValue={contacts?.email} onChange={(e)=> { setContacts({...contacts,email : e.target.value}) }}  className="form-control"  placeholder='email' />
 								</div>  
 							</div>
 							
 							<div className="mb-3">
 								<label  className="form-label">Address</label>
 								<div className='d-flex gap-3'>
-								  	<input type="text"  defaultValue={contacts?.address}  className="form-control"  placeholder='address' />
+								  	<input type="text"  defaultValue={contacts?.address} onChange={(e)=> { setContacts({...contacts,address : e.target.value}) }}  className="form-control"  placeholder='address' />
 								</div>  
 							</div>
 							
-							<button type="submit"  className="btn formSubBtn">Save</button>
+							<button type="submit" onClick={saveContacts}  className="btn formSubBtn">Save</button>
 						</div>
 					</div>
 				</div>
