066e453bb4f9e7af6b5fde4b19e89176a71e161d
MD Ripon Islam <islam876ripon@gmail.com>
2023-04-19 14:12:30 +0600

    logo added

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import './SearchBar.css';
+import React from "react";
+import "./SearchBar.css";
 
 function SearchBar() {
-	return (
-		<div>
-			<div className="container py-3">
-
-            <div className="row height d-flex justify-content-center align-items-center">
-
-              <div className="col-md-6">
-
-                <div className="form">
-                  <i className="fa fa-search"></i>
-                  <input type="text" className="form-control form-input" placeholder="Search anything..." />
-                  <span className="left-pan"><i className="fa fa-microphone"></i></span>
-                </div>
-                
-              </div>
-              
+  return (
+    <div>
+      <div className="container py-3">
+        <div className="row height d-flex justify-content-center align-items-center">
+          <div class="col-md-2">
+            <div>
+              <img src="./logo.png" alt="Fyndo Logo" className="img-fluid" />
             </div>
-            
           </div>
-		</div>
-	)
+          <div className="col-md-6">
+            <div className="form">
+              <i className="fa fa-search"></i>
+              <input
+                type="text"
+                className="form-control form-input"
+                placeholder="Search anything..."
+              />
+              <span className="left-pan">
+                <i className="fa fa-microphone"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
